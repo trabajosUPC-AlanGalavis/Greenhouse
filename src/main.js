97b96b451282ae7d87b8e800c00812774838663d
Nicolas <nespinoza.naer@gmail.com>
2023-09-21 00:20:29 -0500

    feat(process-table): added process-table component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import ToastService from "primevue/toastservice";
 import PrimeVue from "primevue/config";
 // App Theme
 import 'primevue/resources/themes/md-light-indigo/theme.css';
@@ -16,12 +18,16 @@ import Avatar from "primevue/avatar";
 import Menu from "primevue/menu";
 import Menubar from "primevue/menubar";
 import Toolbar from "primevue/toolbar";
-import DataTable from "primevue/datatable"
-import Column from 'primevue/column'
 import Steps from 'primevue/steps';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import Dialog from 'primevue/dialog';
+import InputText from "primevue/inputtext";
 
 createApp(App)
     .use(PrimeVue, { ripple: true })
+    .use(router)
+    .use(ToastService)
     .component('pv-card', Card)
     .component('pv-button', Button)
     .component('pv-select-button', SelectButton)
@@ -30,7 +36,9 @@ createApp(App)
     .component('pv-menu', Menu)
     .component('pv-menubar', Menubar)
     .component('pv-toolbar', Toolbar)
-    .component('pv-data-table', DataTable)
-    .component('pv-column', Column)
     .component('pv-steps', Steps)
+    .component('pv-column', Column)
+    .component('pv-data-table', DataTable)
+    .component('pv-dialog', Dialog)
+    .component('pv-input-text', InputText)
     .mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -9,3 +9,5 @@ const router = createRouter({
     },
     ]
 })
+
+export default router

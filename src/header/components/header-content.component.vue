41e3f362015920f4879f3a4dffbec6f296f0994f
Andres Soto <u202116113@upc.edu.pe>
2023-09-21 20:31:55 -0500

    feat(header): added header component

---
+++ src/header/components/header-content.component.vue
@@ -0,0 +1,41 @@
+<template>
+  <pv-toolbar class="sticky" :style="{ backgroundColor: 'var(--primary-green)', borderRadius: '0' }">
+    <template #start>
+      <a href="">
+        <img src="public/logo-white.png" alt="logo" width="30" height="30">
+        Greenhouse</a>
+      <a href="">
+        Panel de control</a>
+      <a href="">
+        Cultivos</a>
+    </template>
+    <template #end>
+      <a href="">
+        <img src="public/logo-white.png" alt="logo" width="30" height="30">
+        Empresa</a>
+      <a href="">
+        <img src="public/logo-white.png" alt="logo" width="30" height="30">
+        Usuario</a>
+    </template>
+  </pv-toolbar>
+</template>
+
+<script>
+</script>
+
+<style>
+  body {
+    margin: 0 !important;
+  }
+  a{
+    text-decoration: none;
+    color: var(--primary-white);
+    display: flex;
+    align-items: center;
+    margin-right: 40px;
+  }
+
+  .link-text {
+    margin-left: 8px;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import RecordsContent from "@/harvestings/components/records-content.component.vue";
 import HarvestList from "@/process_log/components/harvest-list.component.vue";
 
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [

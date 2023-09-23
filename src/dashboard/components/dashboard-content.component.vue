c8bfea097ae3e939cb8c835cb46bc8b7aeffad5a
Alan Galavis <102612482+trabajosUPC-AlanGalavis@users.noreply.github.com>
2023-09-23 16:50:00 -0500

    fix(dashboard): Centered images content

--- src/dashboard/components/dashboard-content.component.vue
+++ src/dashboard/components/dashboard-content.component.vue
@@ -1,15 +1,15 @@
 <template>
-  <pv-card class="m-6">
+  <pv-card class="card">
     <template #title>
-      <h2 class="p-4">Dashboard</h2>
+      <h2>Dashboard</h2>
     </template>
     <template #content>
-      <div class="text-center"> <!-- Cambia "content-center" por "text-center" -->
-        <div class="grid md:grid-cols-12 gap-4 pb-5 justify-items-center">
-          <dashboard-card class="col-span-3" :image="'public/dashboard/process-state.png'" :buttonText="'Harvesting in progress'" :link="'/process-log'"></dashboard-card>
-          <dashboard-card  class="col-span-3" :image="'public/dashboard/enterprise.png'" :buttonText="'My organization'" :link="'/process-log'"></dashboard-card>
-          <dashboard-card  class="col-span-3" :image="'public/dashboard/reports.png'" :buttonText="'Statistical reports'" :link="'/process-log'"></dashboard-card>
-          <dashboard-card  class="col-span-3" :image="'public/dashboard/history.png'" :buttonText="'Harvest archive'" :link="'/process-log'"></dashboard-card>
+      <div class="text-center">
+        <div class="grid gap-4">
+          <dashboard-card class="col-span-3 mx-auto" :image="'public/dashboard/process-state.png'" :buttonText="'Harvesting in progress'" :link="'/process-log'"></dashboard-card>
+          <dashboard-card  class="col-span-3 mx-auto" :image="'public/dashboard/enterprise.png'" :buttonText="'My organization'" :link="'/process-log'"></dashboard-card>
+          <dashboard-card  class="col-span-3 mx-auto" :image="'public/dashboard/reports.png'" :buttonText="'Statistical reports'" :link="'/process-log'"></dashboard-card>
+          <dashboard-card  class="col-span-3 mx-auto" :image="'public/dashboard/history.png'" :buttonText="'Harvest archive'" :link="'/process-log'"></dashboard-card>
         </div>
       </div>
     </template>
@@ -28,16 +28,23 @@ export default {
 </script>
 
 <style scoped>
+.card {
+  background-color: var(--white);
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 1px 0 rgba(0, 0, 0, 0.20);
+  border-radius: 20px;
+  margin: 2.7rem 9rem;
+}
+
 h2 {
   text-align: center;
   font-family: var(--font-primary);
   font-size: var(--heading-2-size);
-  font-style: normal;
-  font-weight: 700;
-  line-height: 49px;
 }
 
-pv-card {
-  border-radius: 20px;
+@media(max-width: 768px) {
+  .card {
+    margin: 2.7rem 2rem;
+  }
 }
+
 </style>

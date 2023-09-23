e809784dc3e4e128fdf48fe93ecb2962e0025975
Jess_2610 <105735491+Mei0202@users.noreply.github.com>
2023-09-23 16:32:32 -0500

    feat<router>: Path popupcomponent add for see the component

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HarvestList from "@/process_log/components/harvest-list.component.vue";
 import StepperContent from "@/harvestings/components/stepper-content.component.vue";
-
+import PopupWarningComponent from "@/shared/components/popup-warning.component.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +21,10 @@ const router = createRouter({
         {
             path: "/stepper",
             component: StepperContent,
+        },
+        {
+            path:"/popup-warning-component",
+            component:PopupWarningComponent,
         },
     ],
 });

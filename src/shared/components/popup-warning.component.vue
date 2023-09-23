3a04e5c39a11d6fc3ebe623a37d2d9a064cbffaa
Jess_2610 <105735491+Mei0202@users.noreply.github.com>
2023-09-23 16:37:21 -0500

    feat(popup script): openpopup and closepopup functions add

--- src/shared/components/popup-warning.component.vue
+++ src/shared/components/popup-warning.component.vue
@@ -1,5 +1,21 @@
-<script setup>
-
+<script>
+export default {
+  data() {
+    return {
+      showPopup: false,
+      popupText:
+          "This phase is completed, you will not be able to make any more records at this stage. Are you sure you want to continue?",
+    };
+  },
+  methods: {
+    openPopup() {
+      this.showPopup = true;
+    },
+    closePopup() {
+      this.showPopup = false;
+    },
+  },
+};
 </script>
 
 <template>

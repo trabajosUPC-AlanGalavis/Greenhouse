e9b8a91cd09d63aed12031bdffb3c13c7cf56d04
Jess_2610 <105735491+Mei0202@users.noreply.github.com>
2023-09-23 16:38:12 -0500

    feat(popuphtml): HTML add

--- src/shared/components/popup-warning.component.vue
+++ src/shared/components/popup-warning.component.vue
@@ -19,7 +19,23 @@ export default {
 </script>
 
 <template>
-
+  <button class="btn" @click="openPopup" :style="{ color: 'black' }">Confirm</button>
+  <div class="popup-container" v-if="showPopup">
+    <div class="popup-content">
+      <div class="popup-header">
+        <h2>WARNING</h2>
+      </div>
+      <div class="popup-body">
+        <br>
+        <p style="text-align: center;">{{ popupText }}</p>
+        <h5>This operation is irreversible</h5>
+      </div>
+      <div class="popup-footer">
+        <button class="btn yes-finish" @click="closePopup()">Yes, finish it</button>
+        <button class="btn close" @click="closePopup()">Cancel</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped>

f8fc96785dd595bf90c17ad43de78656cb607f52
CarloLSG <u20211a475@upc.edu.pe>
2023-09-24 01:44:53 -0500

    fix: deleted duplicate import router.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
 import ToastService from "primevue/toastservice";
 import PrimeVue from "primevue/config";
 // App Theme

ca17689a13a7146819973479ef751cb0cfe7c17e
CarloLSG <u20211a475@upc.edu.pe>
2023-10-20 09:54:18 -0500

    feat(process-input-dialog): the data is now post correctly.

--- src/greenhouse/components/process-input-dialog.component.vue
+++ src/greenhouse/components/process-input-dialog.component.vue
@@ -12,7 +12,7 @@
       <div class="p-fluid p-2">
         <div class="p-field" v-for="title in titles" :key="title.field">
           <h1>{{ title.header }}</h1>
-          <input class="p-inputtext" :placeholder="'Register ' + title.header" />
+          <input class="p-inputtext" :placeholder="'Register ' + title.header" v-model="formData[title.field]"/>
         </div>
       </div>
       <div class="p-dialog-footer flex">
@@ -42,7 +42,8 @@ export default {
       filters: {},
       processData: [],
       titles: null,
-      greenhouseApi: new GreenhouseApiService()
+      greenhouseApi: new GreenhouseApiService(),
+      formData: {}
     }
   },
   created() {
@@ -67,31 +68,118 @@ export default {
     closeDialog() {
       this.displayDialog = false;
     },
-    saveDialog() {
+    getDataToSend() {
       const currentDateTime = new Date();
-
       const currentDate = currentDateTime.toISOString().split('T')[0];
       const currentTime = currentDateTime.toTimeString().split(' ')[0];
-      const dataToSend = {
-        author: this.author,
-        day: this.day,
+
+      // Common data fields
+      const commonData = {
+        author: 'Winston Smith',
+        day: this.formData.day, // TODO calculate it
         date: currentDate,
         time: currentTime,
-        thermocoupleOne: this.thermocoupleOne,
-        thermocoupleTwo: this.thermocoupleTwo,
-        thermocoupleThree: this.thermocoupleThree,
-        average: this.average,
-        frequency: this.frequency,
-        comment: this.comment,
+        crop_id: 1, // TODO calculate it
       };
+
+      // Merge common data with the formData
+      //const dataToSend = this.formData;
+      console.log('Data to send:', this.formData);
+      const dataToSend = { ...commonData, ...this.formData };
+
+      // Add specific fields based on the endpoint
+      if (this.endpoint === 'stock?') {
+        dataToSend.hay = this.formData.hay;
+        dataToSend.corn = this.formData.corn;
+        dataToSend.guano = this.formData.guano;
+        dataToSend.cottonSeedCake = this.formData.cottonSeedCake;
+        dataToSend.soyBeanMeal = this.formData.soyBeanMeal;
+        dataToSend.gypsum = this.formData.gypsum;
+        dataToSend.urea = this.formData.urea;
+        dataToSend.ammoniumSulphate = this.formData.ammoniumSulphate;
+        // Add other fields as needed
+      } else if (this.endpoint === 'preparation_area?') {
+        dataToSend.activities = this.formData.activities;
+        dataToSend.temperature = this.formData.temperature;
+        dataToSend.comment = this.formData.comment;
+        // Add other fields as needed
+      } else if (this.endpoint === 'bunker?') {
+        dataToSend.thermocoupleOne = this.formData.thermocoupleOne;
+        dataToSend.thermocoupleTwo = this.formData.thermocoupleTwo;
+        dataToSend.thermocoupleThree = this.formData.thermocoupleThree;
+        dataToSend.average = this.formData.average;
+        dataToSend.frequency = this.formData.frequency;
+        dataToSend.comment = this.formData.comment;
+        // Add other fields as needed
+      } else if (this.endpoint === 'tunnel?') {
+        dataToSend.growRoom = this.formData.growRoom;
+        dataToSend.thermocoupleOne = this.formData.thermocoupleOne;
+        dataToSend.thermocoupleTwo = this.formData.thermocoupleTwo;
+        dataToSend.thermocoupleThree = this.formData.thermocoupleThree;
+        dataToSend.average = this.formData.average;
+        dataToSend.frequency = this.formData.frequency;
+        dataToSend.freshAir = this.formData.freshAir;
+        dataToSend.recirculation = this.formData.recirculation;
+        dataToSend.comment = this.formData.comment;
+        // Add other fields as needed
+      } else if (this.endpoint === 'grow_room_record?processType=Incubation&&') {
+        dataToSend.growRoom = this.formData.growRoom;
+        dataToSend.airTemperature = this.formData.airTemperature;
+        dataToSend.compostTemperature = this.formData.compostTemperature;
+        dataToSend.carbonDioxide = this.formData.carbonDioxide;
+        dataToSend.airHydrogen = this.formData.airHydrogen;
+        dataToSend.setting = this.formData.setting;
+        dataToSend.comment = this.formData.comment;
+        dataToSend.processType = 'Incubation';
+      } else if (this.endpoint === 'grow_room_record?processType=Casing&&') {
+        dataToSend.growRoom = this.formData.growRoom;
+        dataToSend.airTemperature = this.formData.airTemperature;
+        dataToSend.compostTemperature = this.formData.compostTemperature;
+        dataToSend.carbonDioxide = this.formData.carbonDioxide;
+        dataToSend.airHydrogen = this.formData.airHydrogen;
+        dataToSend.setting = this.formData.setting;
+        dataToSend.comment = this.formData.comment;
+        dataToSend.processType = 'Casing';
+      } else if (this.endpoint === 'grow_room_record?processType=Induction&&') {
+        dataToSend.growRoom = this.formData.growRoom;
+        dataToSend.airTemperature = this.formData.airTemperature;
+        dataToSend.compostTemperature = this.formData.compostTemperature;
+        dataToSend.carbonDioxide = this.formData.carbonDioxide;
+        dataToSend.airHydrogen = this.formData.airHydrogen;
+        dataToSend.setting = this.formData.setting;
+        dataToSend.comment = this.formData.comment;
+        dataToSend.processType = 'Induction';
+      } else if (this.endpoint === 'grow_room_record?processType=Harvest&&') {
+        dataToSend.growRoom = this.formData.growRoom;
+        dataToSend.airTemperature = this.formData.airTemperature;
+        dataToSend.compostTemperature = this.formData.compostTemperature;
+        dataToSend.carbonDioxide = this.formData.carbonDioxide;
+        dataToSend.airHydrogen = this.formData.airHydrogen;
+        dataToSend.setting = this.formData.setting;
+        dataToSend.comment = this.formData.comment;
+        dataToSend.processType = 'Harvest';
+      }
+      return dataToSend;
+    },
+
+
+    saveDialog() {
+      const dataToSend = this.getDataToSend();
+      //console.log('Data to send:', dataToSend)
       this.greenhouseApi.create(this.endpoint, dataToSend)
           .then(response => {
             console.log('Data saved successfully:', response.data);
+            this.clearInputFields();
           })
           .catch(error => {
             console.error('Error saving data:', error);
           });
       this.displayDialog = false;
+    },
+    clearInputFields() {
+      for (const key in this.formData) {
+        this.formData[key] = null;
+      }
     },
     initFilters() {
       this.filters = {
@@ -100,8 +188,7 @@ export default {
     },
     addData() {
       this.titles = Object.keys(this.processData[0])
-          .filter((key) => key !== "processType" && key !== 'crop_id' && key !== "id" && key !== "apiId" && key !== "author" && key !== "day" && key !== "date" && key !== "time")
-          //.filter((key) => key !== "processType" && key !== 'crop_id' && key !== "id" && key !== "apiId" && key !== "date" && key !== "time")
+          .filter((key) => key !== "processType" && key !== 'crop_id' && key !== "id" && key !== "apiId" && key !== "author" && key !== "date" && key !== "time")
           .map((key) => {
             const formattedHeader = key
                 .split(/(?=[A-Z])/)

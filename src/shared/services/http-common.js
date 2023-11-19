9e5ac2728cb53cf3c51be310cd649931fbe79dda
CarloLSG <u20211a475@upc.edu.pe>
2023-11-19 16:46:08 -0500

    chore: made the connection with the deployed back-end

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export default axios.create({
     //baseURL: 'https://my-json-server.typicode.com/CarloLSG/GreenhouseFakeAPI2',
-    //baseURL: 'http://greenhouseupc-001-site1.ctempurl.com/api/v1',
-    baseURL: 'https://localhost:7170/api/v1/crops/',
+    baseURL: 'http://greenhouseupc-001-site1.ctempurl.com/api/v1/crops/',
+    //baseURL: 'https://localhost:7170/api/v1/crops/',
 
     headers: { 'Content-type': 'application/json'},
 })
